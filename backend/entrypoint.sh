e896dc5725d7bea90ccdb2c431abe96045e1c344
ozziken <PS.voxys@yandex.ru>
2023-07-08 02:53:19 +0300

    All works. Test workflow.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+sleep 5
+
 python manage.py collectstatic
+
 cp -r /app/collected_static/. /backend_static/static/
+
 python manage.py migrate
+
 gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
