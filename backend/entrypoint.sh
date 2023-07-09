d9348aeb2129e42eaf6ac2ca72664b6d6c84000b
ozziken <PS.voxys@yandex.ru>
2023-07-09 13:57:35 +0300

    Rev2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
 
 python manage.py collectstatic
+
 cp -r /app/collected_static/. /backend_static/static/
+
 python manage.py migrate
+
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
